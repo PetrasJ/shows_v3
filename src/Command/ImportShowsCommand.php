fbeafa91c2f2d934a9039f26dab4a02e516cd5bd
Petras <petras.jodkonis@gmail.com>
2019-05-10 09:10:25 +0300

    add manager

---
+++ src/Command/ImportShowsCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\ShowsManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportShowsCommand extends Command
+{
+    private $showsManager;
+
+    public function __construct(ShowsManager $showsManager)
+    {
+        parent::__construct();
+        $this->showsManager = $showsManager;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this->setName('import-shows')
+            ->setDescription('Imports shows from TV Maze')
+        ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $output->writeln([
+            'Import shows',
+            $this->showsManager->load() ? 'true' : 'false',
+        ]);
+    }
+}
---
+++ src/Service/ShowsManager.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service;
+
+class ShowsManager
+{
+    public function load()
+    {
+        return true;
+    }
+}
