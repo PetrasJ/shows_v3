df07fd13851ff8d30afb7a7cd1e26d524f2a503c
Petras <petras.jodkonis@gmail.com>
2019-06-16 12:10:54 +0300

    add user episode service test

---
+++ tests/Service/UserEpisodeServiceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Episode;
+use App\Entity\Show;
+use App\Entity\User;
+
+use App\Entity\UserEpisode;
+use App\Entity\UserShow;
+use App\Repository\UserEpisodeRepository;
+use App\Repository\UserShowRepository;
+use App\Service\Storage;
+use App\Service\UserEpisodeService;
+use Doctrine\ORM\EntityManager;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class UserEpisodeServiceTest extends TestCase
+{
+    public function testUpdate()
+    {
+        /** @var EntityManager|MockObject $entityManager */
+        $entityManager = $this->createMock(EntityManager::class);
+
+        $episode = (new Episode())->setShow(new Show());
+        $entityManager->method('find')->willReturn($episode);
+        $userEpisodeRepo = $this->createMock(UserEpisodeRepository::class);
+        $userShowRepo = $this->createMock(UserShowRepository::class);
+        $userShowRepo->method('findOneBy')->willReturn((new UserShow()));
+
+        $entityManager
+            ->expects($this->any())
+            ->method('getRepository')
+            ->willReturnOnConsecutiveCalls($userEpisodeRepo, $userShowRepo)
+        ;
+
+        $storage = new Storage();
+        $storage->setUser((new User()));
+        $service = new UserEpisodeService($entityManager, $storage);
+
+        $entityManager->expects($this->once())
+            ->method('persist')
+            ->with($this->isInstanceOf(UserEpisode::class));
+
+        $service->update(1, 1, ['watch' => true]);
+    }
+}
