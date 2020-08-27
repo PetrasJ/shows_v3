ccad24cddec64d6265b7c58f936284839c8f49eb
Petras <petras.jodkonis@gmail.com>
2020-08-28 00:02:08 +0300

    rename table

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @ORM\Entity
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity(fields={"email"}, message="there_is_already_an_account_with_this_email")
- * @ORM\Table(name="fos_user")
+ * @ORM\Table(name="user")
  */
 class User implements UserInterface
 {
